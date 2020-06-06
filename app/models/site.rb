a05868c769833bc71adf96193119d7abd3d9a58c
Nick Weber <webernick@icloud.com>
2020-06-06 10:32:51 -0700

    Default image generator concern

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,4 +1,13 @@
 class Site < ApplicationRecord
+
+  includes DefaultImage
+
   belongs_to :project
   has_many :deployments
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.image ||= DefaultImage.default_image_generator(height: '400', width: '300')
+  end
 end
